a592196b7a4128f55410376dd4b7a9fcade1ce16
Alexis Fecteau <newbiedawannabe@gmail.com>
2024-02-26 11:45:32 -0500

    Test du directoire dézippé. confirmer que ça marche avec tout le monde avant de mettre au main.

---
+++ Newtonium/core/src/com/mygdx/locomotor/Item.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mygdx.locomotor;
+
+/**
+ *
+ * @author 2249229
+ */
+public class Item {
+    
+    int level, cooldown;
+    
+    public void trigger(){
+        
+    }
+    
+    
+}
