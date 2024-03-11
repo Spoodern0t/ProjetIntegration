3dbe57ba48c5a5b466f95422aa33cdb4c7b3c441
Adamo2611 <adamo2611@gmail.com>
2024-03-11 15:03:08 -0400

    Update Projectile.java
    classe projectile work in progress

--- Newtonium/core/src/com/mygdx/locomotor/Projectile.java
+++ Newtonium/core/src/com/mygdx/locomotor/Projectile.java
@@ -5,6 +5,9 @@
 package com.mygdx.locomotor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 /**
  *
  * @author Alexis Fecteau (2060238)
@@ -12,14 +15,29 @@ import com.badlogic.gdx.graphics.Texture;
 public class Projectile extends Entity{
     
     double flatDamage, decayTime;
+    public float vitesse_projectile;
+    public Sprite sprite_projectile;
     
-    public Projectile(){
+    public Projectile(float x, float y, float speed, float angle, Texture texture){
         this.maxHP = maxHP;
+        position= new Vector2(0,1000);
+        this.position = new Vector2(x, y);
+       this.position = new Vector2(x, y);
+        this.speed = speed;
+        this.position = new Vector2(speed * (float)Math.cos(angle), speed * (float)Math.sin(angle));
+        this.sprite = new Sprite(texture); 
+        this.sprite.setPosition(x, y);
+    
         
     }
     
-    public void update(){
+    public void update(float deltaTime){
+         position.add(position.x * deltaTime, position.y * deltaTime);
+        sprite.setPosition(position.x, position.y);
+    }
+    public void draw(SpriteBatch batch) {
         
+        sprite.draw(batch);
     }
     
     /*
