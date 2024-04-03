b476f57d76bf73395b7a351237730dbd7abb0693
Benexit <Benexit@outlook.com>
2024-04-03 09:34:10 -0400

    Create Global.java

---
+++ Newtonium/core/src/com/mygdx/locomotor/Global.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mygdx.locomotor;
+
+import com.badlogic.gdx.graphics.Texture;
+
+
+/**
+ *
+ * @author Thomas Cyr 
+ */
+public class Global {
+    
+    
+    
+    public static class Textures {
+   
+        //creating the textures
+        public static Texture hurt =new Texture("sadge.png");
+        
+        public static Texture idlePlayer = new Texture("LilBoy.png");
+        
+        public static Texture evil = new Texture("Evil.png");
+        
+        public static Texture map = new Texture("MapImg.jpg");
+        
+    }
+    
+    //Put all the different types of enemy objects and their attributes (for later mostly)
+    public static class Enemies {
+        
+    }
+    
+    //Put all the different types of projectile objects and their attributes (for later mostly)
+    public static class Projectiles {
+        
+    }
+    
+    //Put all the different item objects and their attributes (for later mostly)
+    public static class items {
+        
+    }
+    
+}
