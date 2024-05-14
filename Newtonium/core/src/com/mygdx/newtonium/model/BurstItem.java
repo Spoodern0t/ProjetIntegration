3b4adadf7f56c384016894734f8bb56fa697ce75
Alexis Fecteau <newbiedawannabe@gmail.com>
2024-05-14 10:25:56 -0400

    New prebuilt Items & last second cleanup

--- Newtonium/core/src/com/mygdx/newtonium/model/BurstItem.java
+++ Newtonium/core/src/com/mygdx/newtonium/model/BurstItem.java
@@ -15,7 +15,7 @@ public class BurstItem extends Item {
     
 //attributes
     int amount;
-    private static int triggerCounter = 0;
+    private int triggerCounter = 0;
     
 //constructors
     public BurstItem(int level, float cooldown, Projectile projectile, int amount){
@@ -26,21 +26,22 @@ public class BurstItem extends Item {
 //methods
     @Override
     protected void update(float deltaTime){
+        
         this.lastTriggerTime += deltaTime;
-        if (this.canTrigger()){
-            this.trigger();
-            triggerCounter++;
-        }
         
         if (this.lastTriggerTime >= this.cooldown){
             this.lastTriggerTime = 0;
             this.triggerCounter = 0;
         }
+                
+        if (this.canTrigger()){
+            this.trigger();
+        }
     }
     
     @Override
     protected boolean canTrigger() {
-        float burstLength = 1f; //first half of cooldown
+        float burstLength = 1f; //all projectiles fire within this time span
         
         if (triggerCounter < amount){
             return (this.lastTriggerTime >= ((triggerCounter)*(burstLength/this.amount)));
@@ -50,6 +51,7 @@ public class BurstItem extends Item {
     @Override
     protected void trigger(){
         GameScreen.projectileList.add((Projectile)this.projectile.spawn());
+        this.triggerCounter++;
         
     }
     
